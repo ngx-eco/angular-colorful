4eebf65f59c8aabf1ec87c22f9e1df7f9ba069f4
fil0157 <56268081+fil0157@users.noreply.github.com>
2020-09-28 12:46:33 +0300

    Import project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
+// Angular
 import { Component } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-colorful';
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
+// Angular
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+// App
 import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+
+// Server
+import { ServiceWorkerModule } from '@angular/service-worker';
+
+// Project
+import { AngularColorfulModule } from 'angular-colorful';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AngularColorfulModule,
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
