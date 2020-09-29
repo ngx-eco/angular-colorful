8d8ebcad89e3c5e608cda3c9af9e39a09824cb02
fil0157 <56268081+fil0157@users.noreply.github.com>
2020-09-29 12:13:44 +0300

    Interactive component created

--- src/angular-colorful/src/lib/angular-colorful.module.ts
+++ src/angular-colorful/src/lib/angular-colorful.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 // Project
 import { ColorPickerComponent } from './components/color-picker/color-picker.component';
+import { InteractiveComponent } from './components/common/interactive/interactive.component';
 
 
 
 @NgModule({
-  declarations: [ColorPickerComponent],
+  declarations: [
+    ColorPickerComponent,
+    InteractiveComponent,
+  ],
   imports: [],
-  exports: [ColorPickerComponent]
+  exports: [
+    ColorPickerComponent,
+  ]
 })
 export class AngularColorfulModule { }
---
+++ src/angular-colorful/src/lib/components/common/interactive/interactive.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'interactive',
+  templateUrl: './interactive.component.html',
+  styleUrls: ['./interactive.component.scss']
+})
+export class InteractiveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
